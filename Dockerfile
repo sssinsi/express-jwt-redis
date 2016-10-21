f979a94b81f765fcc6060e4c31a1816115c8faff
sssinsi <tet643773@gmail.com>
2016-10-21 11:55:04 +0900

    add express app

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
 FROM node:6.9.1
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY ./express_app/package.json /usr/src/app/
+RUN npm install
+
+COPY ./express_app /usr/src/app/
+
 EXPOSE 8888
 
+CMD ["npm", "start"]
---
+++ express_app/index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+app.set('port',8888);
+app.get('/', function(request, response){
+    response.send('Hello World!')
+});
+
+app.listen(app.get('port'), function(){
+    console.log("Node app is running at localhost:" + app.get('port'))
+});
