9519564e0ce41faf81c066416655a41c5bc44c35
sssinsi <tet643773@gmail.com>
2016-10-28 16:11:26 +0900

    add run bush

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker build -t my-express-app .
 docker run --link redis:db -p 9999:8888 -p 9000:8000 -d my-express-app
+docker run --link redis:db -p 9999:8888 -p 9000:8000 -it my-express-app /bin/bash
